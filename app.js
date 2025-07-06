8e63869a2ee234bad0b85f7b8df7e33bdcb22612
sarthak <sarthakpomane123@gmail.com>
2025-07-07 00:12:03 +0530

    Api integrated

---
+++ app.js
@@ -0,0 +1,64 @@
+const BASE_URL = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_xA91UDx5VtGlmOz6jVQRphJ3rGCfN8x68Yp5Awzm";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+// Populate dropdown options with currency codes
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  // Fetch data from the API
+  let response = await fetch(BASE_URL);
+  let data = await response.json();
+
+  // Convert amount using exchange rate from the response
+  let fromRate = data.data[fromCurr.value];
+  let toRate = data.data[toCurr.value];
+  let rate = toRate / fromRate;
+  let finalAmount = amtVal * rate;
+
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
